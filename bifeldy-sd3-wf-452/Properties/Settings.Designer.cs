62dc114736685d8abccf7ff4de0e6dfd76a6b332
B. Bias A. Ch <bias@indomaret.co.id>
2023-09-29 12:39:46 +0700

    Web Service ~

--- bifeldy-sd3-wf-452/Properties/Settings.Designer.cs
+++ bifeldy-sd3-wf-452/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace KirimNPFileQR.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,16 @@ namespace KirimNPFileQR.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://172.24.16.201:1414/service.asmx")]
+        public string KirimNPFileQR_wsNPLtoko_NPB_Service {
+            get {
+                return ((string)(this["KirimNPFileQR_wsNPLtoko_NPB_Service"]));
             }
         }
     }
